27ed10a791b8ee03e42d34b690aac39fdae585cb
Sk Sadman Sayeed <tasinsayeed117@outlook.com>
2020-07-21 22:05:54 -0400

    added Parties to navbar for hopefully every page

--- catchPokemon.php
+++ catchPokemon.php
@@ -64,6 +64,7 @@ if (!isset($_SESSION['name']) || !isset($_SESSION['uid'])) {
     <li class="nav-item"><a class="nav-link" href="pokemon.php"> Search Pokemon </a></li>
     <li class="nav-item"><a class="nav-link" href="catchPokemon.php"> Catch Pokemon </a></li>
     <li class="nav-item"><a class="nav-link" href="organizePokemon.php"> Organize Pokemon </a></li>
+	<li class="nav-item"><a class="nav-link" href="partyshare.php"> Parties </a></li>
 	<li class="nav-item"><a class="nav-link" href="profile.php"><span class="fa fa-user"></span> <?php echo $_SESSION['name'];?></a></li>
 	<li class="nav-item"><a class="nav-link" href="index.php"><span class="fa fa-sign-out"></span> Logout</a></li>
       </ul>
--- organizePokemon.php
+++ organizePokemon.php
@@ -176,6 +176,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <li class="nav-item"><a class="nav-link" href="pokemon.php"> Search Pokemon </a></li>
     <li class="nav-item"><a class="nav-link" href="catchPokemon.php"> Catch Pokemon </a></li>
     <li class="nav-item"><a class="nav-link" href="organizePokemon.php"> Organize Pokemon </a></li>
+	<li class="nav-item"><a class="nav-link" href="partyshare.php"> Parties </a></li>
 	<li class="nav-item"><a class="nav-link" href="profile.php"><span class="fa fa-user"></span> <?php echo $_SESSION['name'];?></a></li>
 	<li class="nav-item"><a class="nav-link" href="index.php"><span class="fa fa-sign-out"></span> Logout</a></li>
       </ul>
