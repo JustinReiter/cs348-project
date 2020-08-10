bd8adc4a299f7b4f56131bafa93272c82be990ca
wu-benjamin <benjaminwu24@gmail.com>
2020-08-10 15:45:41 -0400

    Remove duplicate sql script after archive

--- CreateTableSQLQueries/createFavouritePokemonTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE favourite_pokemon
-(
-	uid integer NOT NULL,
-	pid integer NOT NULL,
-	PRIMARY KEY(pid, uid),
-	FOREIGN KEY(uid) REFERENCES player(uid),
-	FOREIGN KEY(pid) REFERENCES pokemon(pid)
-);
